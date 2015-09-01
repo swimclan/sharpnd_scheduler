0dca4774f07e1450110dfa86657cb6930dbfc377
Matthew Herron <matthew.herron77@gmail.com>
2015-09-01 11:53:43 -0500

    seeded the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create the default keys
 Key.create(:secret_key => 'key123', :admin => true)
+
+# Create the default products
+Product.create(:product_name => 'Grooming')
+Product.create(:product_name => 'Alterations Tailoring')
+Product.create(:product_name => 'Repair Tailoring')
+Product.create(:product_name => 'Made to Measure')
