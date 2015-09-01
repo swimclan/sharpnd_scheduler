ccb2ac5e42ec93eb059e689b946b563168c2a348
Matthew Herron <matthew.herron77@gmail.com>
2015-09-01 09:31:54 -0500

    Added a seed job to put in a default API admin key

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Key.create(:secret_key => 'key123', :admin => true)
